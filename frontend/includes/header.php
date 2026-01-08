9642927a33ac21fb1df2370b226f7bafabd8b8d8
Fernando Camacho <f.eliomar.2480@gmail.com>
2026-01-08 01:30:54 -0500

    estructura final
    se corrigió la estructura del proyecto, también se crearon las páginas  y los layouts

---
+++ frontend/includes/header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teatro Andino Kusiwasi</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/estilos_index.css">
+</head>
+<body>
+
+<header class="header">
+    <div class="header-overlay">
+        <div class="container">
+            <div class="row align-items-center py-3">
+                <div class="col-md-2 text-center text-md-start">
+                    <a href="../index.php" class="logo">
+                        <img src="../assets/img/logo.png" alt="Logo Kusiwasi">
+                    </a>
+                </div>
+                <nav class="col-md-10">
+                    <ul class="nav-links">
+                        <li><a href="../index.php">Inicio</a></li>
+                        <li><a href="../pages/cartelera.php">Shows</a></li>
+                        <li><a href="../pages/tienda.php">Tienda</a></li>
+                        
+                        <!-- Submenú Proyectos -->
+                        <li class="has-submenu">
+                            <a href="../pages/proyectos.php">Proyectos</a>
+                            <ul class="submenu">
+                                <li><a href="#">Taller de Teatro</a></li>
+                                <li><a href="#">Taller de Música</a></li>
+                            </ul>
+                        </li>
+
+                        <!-- Submenú Nosotros -->
+                        <li class="has-submenu">
+                            <a href="../pages/nosotros.php">Nosotros</a>
+                            <ul class="submenu">
+                                <li><a href="../pages/historia.php">Historia</a></li>
+                                <li><a href="../pages/misionvision.php">Misión y Visión</a></li>
+                            </ul>
+                        </li>
+
+                        <li><a href="../pages/legal.php">Legal</a></li>
+                        
+                        <!-- Icono Login -->
+                        <li>
+                            <a href="#" class="login-circle">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                                </svg>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
