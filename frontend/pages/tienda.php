e21e2d3061f9bbf75abc625892023b0704a6400a
Anthony-sutta <AnthonySuttaAtayupanqui@gmail.com>
2026-01-08 17:25:19 -0500

    Tiendas-estilos
    1.agregue los estilos de la tienda en v1
    2.agregue el archivo js para los demas
    3.agregue el automatico scroll para que el usuario no demore en subir al foorter

--- frontend/pages/tienda.php
+++ frontend/pages/tienda.php
@@ -1,7 +1,25 @@
 <?php include '../includes/header.php'; ?>
-
-<main class="container my-5">
-    <h1 class="text-white text-center">Bienvenidos al Teatro Andino</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../assets/css/estios_tienda.css">
+    <link rel="stylesheet" href="../assets/css/estilos_flecha.css">
+</head>
+<body>
+<main>
+    <div class="imgtienda">
+        <img src="../assets/img/imgcarrusel1.png" alt="">
+    </div>
+    <div class="titulo">
+        <p>Recuerdos que mantienen viva la función</p>
+    </div>
 </main>
-
+    </main>
+    <button class="scroll-arrow" id="scrollArrow" aria-label="Subir">↑</button>
+    <script src="../assets/js/scroll-arrow.js"></script>
+</body>
+</html>
 <?php include '../includes/footer.php'; ?>
