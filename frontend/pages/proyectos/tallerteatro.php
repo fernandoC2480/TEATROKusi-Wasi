a348cf96f4fc50073fae3dde8d60dc19dadbba34
Fernando Camacho <f.eliomar.2480@gmail.com>
2026-02-07 20:03:24 -0500

    aa

--- frontend/pages/proyectos/tallerteatro.php
+++
@@ -1,8 +0,0 @@
-<?php include '../../includes/header.php'; ?>
-
-<main class="container my-5">
-    <h1 class="text-white text-center">Bienvenidos al Teatro Andino</h1>
-</main>
-<button class="scroll-arrow" id="scrollArrow" aria-label="Subir">↑</button>
-<script src="../../assets/js/scroll-arrow.js"></script>
-<?php include '../../includes/footer.php'; ?>
